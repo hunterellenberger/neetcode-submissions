7a62ad56b80d273217eef49626e959bdc5f29a69
Hunter Ellenberger <142809944+hunterellenberger@users.noreply.github.com>
2026-05-20 01:40:28 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string return_string = "";
+        string pound = "#";
+
+        for (auto str: strs){
+            return_string += to_string(str.length());
+            return_string += pound;
+            for (auto ascii: str){
+                return_string += ascii;
+            }
+        }
+
+        return return_string;
+    }
+
+    // Get number before hash
+    // Ignore hash and start parsing until previous number is reached
+    // append that string to vector
+    // repeat until end of string
+
+    vector<string> decode(string s) {
+        vector<string> return_vec;
+        int iter = 0;
+
+        while(iter < s.length()){
+            string number = "";
+
+            while(s[iter] != '#'){
+                number += s[iter];
+                iter++;
+            }
+            iter++;
+
+            string word;
+            for(int _ = 0; _ < stoi(number); _++){
+                word += s[iter];
+                iter++;
+            }
+
+            return_vec.push_back(word);
+        }
+
+        return return_vec;
+    }
+};
